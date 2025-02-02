30a8bc980a97fa57180b3bcfdc5c5b3777aecbfa
Hashmi Hashmi <119hashmihashmi@gmail.com>
2025-02-02 16:47:55 +0500

    improved code

--- Typescript code/script.js
+++ Typescript code/script.js
@@ -1,9 +1,17 @@
 function toggleSection() {
-    var h2 = document.getElementById('skills-details');
+    var section = document.getElementById('skills-details');
     var arrow = document.getElementById('skills-arrow');
-    if (h2 && arrow) {
-        var isHidden = h2.style.display === "none";
-        h2.style.display = isHidden ? 'block' : 'none';
-        arrow.style.transform = isHidden ? 'rotate(180deg)' : 'rotate(0deg)';
+    if (section && arrow) { // Check if elements exist
+        if (section.style.display === "none") {
+            section.style.display = "block";
+            arrow.style.transform = "rotate(180deg)";
+        }
+        else {
+            section.style.display = "none";
+            arrow.style.transform = "rotate(0deg)";
+        }
+    }
+    else {
+        console.error("Element not found.");
     }
 }
--- Typescript code/script.ts
+++ Typescript code/script.ts
@@ -1,9 +1,16 @@
 function toggleSection() {
-    const h2 = document.getElementById('skills-details');
-    const arrow = document.getElementById ('skills-arrow')
-    if (h2 && arrow) {
-        const isHidden = h2.style.display === "none";
-        h2.style.display = isHidden ? 'block' : 'none';
-        arrow.style.transform = isHidden? 'rotate(180deg)' : 'rotate(0deg)'
+    const section = document.getElementById('skills-details');
+    const arrow = document.getElementById ('skills-arrow');
+
+    if (section && arrow) { // Check if elements exist
+        if (section.style.display === "none") {
+            section.style.display = "block";
+            arrow.style.transform = "rotate(180deg)";
+        } else {
+            section.style.display = "none";
+            arrow.style.transform = "rotate(0deg)";
+        }
+    } else {
+        console.error("Element not found.");
     }
 }
