ad505ae67581b67f7d81c397fa97159ff0cec553
Hashmi Hashmi <119hashmihashmi@gmail.com>
2025-02-03 09:53:25 +0500

    Imporoved code

---
+++ script.js
@@ -0,0 +1,24 @@
+// function toggleSection() {
+//     const section = document.getElementById('skills-details');
+//     const arrow = document.getElementById ('skills-arrow');
+//     if (section && arrow) { // Check if elements exist
+//         if (section.style.display === "none") {
+//             section.style.display = "block";
+//             arrow.style.transform = "rotate(180deg)";
+//         } else {
+//             section.style.display = "none";
+//             arrow.style.transform = "rotate(0deg)";
+//         }
+//     } else {
+//         console.error("Element not found.");
+//     }
+// }
+function toggleSection(h2Id) {
+    var h2 = document.getElementById(h2Id);
+    var arrow = document.getElementById('skills-arrow');
+    if (h2 && arrow) { // Check if elements exist
+        var isHidden = h2.style.display === "none";
+        h2.style.display = isHidden ? 'block' : 'none';
+        arrow.style.transform = isHidden ? 'rotate(180deg)' : 'rotate(0deg)';
+    }
+}
